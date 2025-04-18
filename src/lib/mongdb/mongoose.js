0f11aac8418f9a86b2b1596ba45d8e3805f0b0d9
Edwards-Alex <1032607679w@gmail.com>
2025-04-18 20:12:05 +0800

    mongoose.set('stricQuery',true); is incorrect rewrite mongoose.set('strictQuery',true);

--- src/lib/mongdb/mongoose.js
+++ src/lib/mongdb/mongoose.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 let initialized = false;
 export const connect = async() => {
-    mongoose.set('stricQuery',true);
+    mongoose.set('strictQuery',true);
     if(initialized){
         console.log('MongoDB already connected');
         return;
